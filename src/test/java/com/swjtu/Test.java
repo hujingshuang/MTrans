cb632062278f25395009e463ad8577fac10b1066
HJS <jingshuang_hu@163.com>
2018-05-23 15:11:53 +0800

    update Test Demo

--- src/test/java/com/swjtu/Test.java
+++ src/test/java/com/swjtu/Test.java
@@ -1,28 +1,46 @@
 package com.swjtu;
 
-import com.swjtu.lang.Lang;
+import com.swjtu.lang.LANG;
 import com.swjtu.querier.Querier;
-import com.swjtu.trans.*;
+import com.swjtu.trans.AbstractTranslator;
+import com.swjtu.trans.impl.*;
+import com.swjtu.tts.AbstractTTS;
+import com.swjtu.tts.impl.*;
 
 import java.util.List;
 
 public class Test {
     public static void main(String[] args) {
-        Querier querier = Querier.getQuerier();
+        // TTS
+        Querier<AbstractTTS> querierTTS = new Querier<>();
+        querierTTS.setParams(LANG.EN, "To be or not to be, that is a question.");
 
-        querier.setParams(Lang.EN, Lang.ZH, "Happiness is a way station between too much and too little.");
+        querierTTS.attach(new BaiduTTS());
+        querierTTS.attach(new YoudaoTTS());
+        querierTTS.attach(new GoogleTTS());
+        querierTTS.attach(new TencentTTS());
+        querierTTS.attach(new SogouTTS());
 
-        querier.attach(new Baidu());
-        querier.attach(new Youdao());
-        querier.attach(new Google());
-        querier.attach(new Tencent());
-        querier.attach(new Omi());
-        querier.attach(new Trycan());
-        querier.attach(new Iciba());
-        querier.attach(new Sogou());
+        List<String> resultTTS = querierTTS.execute();
+        for (String str : resultTTS) {
+            System.out.println(str);
+        }
 
-        List<String> result = querier.execute();
-        for (String str : result) {
+        // translator
+        Querier<AbstractTranslator> querierTrans = new Querier<>();
+        querierTrans.setParams(LANG.EN, LANG.ZH,"Happiness is a way station between too much and too little.");
+
+        querierTrans.attach(new BaiduTranslator());
+        querierTrans.attach(new YoudaoTranslator());
+        querierTrans.attach(new GoogleTranslator());
+        querierTrans.attach(new TencentTranslator());
+        querierTrans.attach(new OmiTranslator());
+        querierTrans.attach(new TrycanTranslator());
+        querierTrans.attach(new IcibaTranslator());
+        querierTrans.attach(new SogouTranslator());
+
+        List<String> resultTrans = querierTrans.execute();
+        for (String str : resultTrans) {
             System.out.println(str);
         }
     }
